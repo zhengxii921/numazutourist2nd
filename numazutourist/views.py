26422bf33816efb43c75974fa3691f8edf78eba2
zhengxii921 <aqua.inm@gmail.com>
2022-04-12 13:03:17 +0900

    Django-axesを追加

--- numazutourist/views.py
+++ numazutourist/views.py
@@ -100,21 +100,21 @@ class UserUpdateView(generic.UpdateView):
 from django.views.decorators.csrf import requires_csrf_token
 from django.http import HttpResponseServerError
 
-
+"""
 @requires_csrf_token
 def my_customized_server_error(request, template_name='500.html'):
     from django.views.defaults import server_error
     return server_error(request, template_name )
+"""
 
-
-""" DEBUG=Falseでエラーを確認するための関数, 使う場合は上をコメントアウト
+""" DEBUG=Falseでエラーを確認するための関数, 使う場合は上をコメントアウト """
 @requires_csrf_token
 def my_customized_server_error(request, template_name='500.html'):
     import sys
     from django.views import debug
     error_html = debug.technical_500_response(request, *sys.exc_info()).content
     return HttpResponseServerError(error_html)
-"""
+
 
 def like(request, pk):
     lovenuma = get_object_or_404(Lovenuma, pk=pk)
